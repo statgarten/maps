dc16dcd1213f4e3715de7e7cbc9bf4af79fc91d7
Dongook Son <60206749+donny-son@users.noreply.github.com>
2022-11-03 22:50:31 +0900

    add web interface with Svelte

---
+++ web/src/App.svelte
@@ -0,0 +1,49 @@
+<script lang="ts">
+  import InlineSVG from "svelte-inline-svg";
+  const maps = [
+    { name: "강원도 시군구 경계", fileName: "강원도_시군구_경계.svg" },
+    { name: "경기도 시군구 경계", fileName: "경기도_시군구_경계.svg" },
+    { name: "경상남도 시군구 경계", fileName: "경상남도_시군구_경계.svg" },
+    { name: "경상북도 시군구 경계", fileName: "경상북도_시군구_경계.svg" },
+    { name: "광주광역시 시군구 경계", fileName: "광주광역시_시군구_경계.svg" },
+    { name: "대구광역시 시군구 경계", fileName: "대구광역시_시군구_경계.svg" },
+    { name: "대전광역시 시군구 경계", fileName: "대전광역시_시군구_경계.svg" },
+    { name: "부산광역시 시군구 경계", fileName: "부산광역시_시군구_경계.svg" },
+    { name: "서울특별시 시군구 경계", fileName: "서울특별시_시군구_경계.svg" },
+    {
+      name: "세종특별자치시 시군구 경계",
+      fileName: "세종특별자치시_시군구_경계.svg",
+    },
+    { name: "울산광역시 시군구 경계", fileName: "울산광역시_시군구_경계.svg" },
+    { name: "인천광역시 시군구 경계", fileName: "인천광역시_시군구_경계.svg" },
+    { name: "전국 시도 경계", fileName: "전국_시도_경계.svg" },
+    { name: "전라남도 시군구 경계", fileName: "전라남도_시군구_경계.svg" },
+    { name: "전라북도 시군구 경계", fileName: "전라북도_시군구_경계.svg" },
+    {
+      name: "제주특별자치도 시군구 경계",
+      fileName: "제주특별자치도_시군구_경계.svg",
+    },
+    { name: "충청남도 시군구 경계", fileName: "충청남도_시군구_경계.svg" },
+    { name: "충청북도 시군구 경계", fileName: "충청북도_시군구_경계.svg" },
+  ];
+  import Footer from "./lib/Footer.svelte";
+</script>
+
+<main class="pb-16">
+  <h1 class="my-8">Simplified Border SVGs for South Korea</h1>
+  <div class="flex flex-col flex-wrap md:flex-row justify-center gap-4">
+    {#each maps as { name, fileName }}
+      <div
+        class="flex flex-col items-center card transition ease-in-out delay-100 hover:scale-110"
+      >
+        <p class="text-dark">{name}</p>
+        <InlineSVG
+          src="https://raw.githubusercontent.com/statgarten/maps/cc92a8feff9817d92805b57ad2368c9c58552841/svg/simple/{fileName}"
+          alt={name}
+          class="w-full h-auto fill-slate-500 stroke-white-900 hover:fill-slate-900 transition ease-in-out delay-10"
+        />
+      </div>
+    {/each}
+  </div>
+</main>
+<Footer />
---
+++ web/src/lib/Footer.svelte
@@ -0,0 +1,36 @@
+<footer
+  class="fixed bottom-0 left-0 z-20 p-4 w-full bg-white border-t border-gray-200 shadow md:flex md:items-center md:justify-between md:p-6 dark:bg-gray-800 dark:border-gray-600"
+>
+  <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400"
+    >© 2022 <a
+      href="https://www.statgarten.com/door"
+      class="hover:underline decoration-white text-white hover:text-white"
+      >Statgarten</a
+    >. All Rights Reserved.
+  </span>
+  <ul
+    class="flex flex-wrap items-center mt-3 text-sm text-gray-500 dark:text-gray-400 sm:mt-0"
+  >
+    <li>
+      <a
+        href="https://github.com/statgarten/maps"
+        class="mr-4 hover:underline decoration-white text-white hover:text-white md:mr-6"
+        >Source Code</a
+      >
+    </li>
+    <li>
+      <a
+        href="https://github.com/statgarten/maps/blob/main/LICENSE"
+        class="mr-4 hover:underline decoration-white text-white hover:text-white md:mr-6"
+        >Licensing</a
+      >
+    </li>
+    <li>
+      <a
+        href="mailto:dongook@bulkrum.kr"
+        class="mr-4 hover:underline decoration-white text-white hover:text-white md:mr-6"
+        >Contact</a
+      >
+    </li>
+  </ul>
+</footer>
